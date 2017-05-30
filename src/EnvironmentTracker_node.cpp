47245bb65a91795ccbc60e6095296abdf54ed7a8
Tobias Fischer <tobias.fischer@rwth-aachen.de>
2017-05-30 18:34:55 +0200

    Action

--- src/EnvironmentTracker_node.cpp
+++ src/EnvironmentTracker_node.cpp
@@ -6,13 +6,13 @@
 #include "geometry_msgs/Pose.h"
 #include "std_srvs/Empty.h"
 
-
 #include <sstream>
 
 class environmentTracker {
 
 private: ros::NodeHandle n;
     ros::Publisher firefly_motor_control_pub;
+    ros::Publisher firefly_control_pub;
     ros::ServiceClient firefly_reset_client;
     ros::Subscriber firefly_position_sub;
 public:
@@ -23,11 +23,17 @@ public:
         n = node;
         firefly_motor_control_pub = n.advertise<mav_msgs::Actuators>("/firefly/command/motor_speed", 1000);
 
+        firefly_control_pub = n.advertise<mav_msgs::RollPitchYawrateThrust>(mav_msgs::default_topics::COMMAND_ROLL_PITCH_YAWRATE_THRUST, 10);
+
         firefly_reset_client = n.serviceClient<std_srvs::Empty>("/gazebo/reset_world");
 
         firefly_position_sub = n.subscribe("/firefly/ground_truth/pose", 1, &environmentTracker::poseCallback, this);
 
         ros::Rate loop_rate(100);
+    }
+
+    void executeAction(Action a) {
+
     }
 
     void respawn() {
@@ -63,17 +69,23 @@ public:
 
 int main(int argc, char **argv)
 {
-
-
-    ros::init(argc, argv, "talker");
+    ros::init(argc, argv, "environmentTracker");
 
     ros::NodeHandle n;
 
-    ros::Publisher chatter_pub = n.advertise<mav_msgs::Actuators>("/firefly/command/motor_speed", 1000);
-    ros::Rate loop_rate(100);
-
     environmentTracker* tracker = new environmentTracker(n);
 
+    ros::Rate loop_rate(100);
+
+    mav_msgs::RollPitchYawrateThrust control_msg;
+    float velocity = atof(argv[1]);
+    float thrustx = atof(argv[2]);
+    control_msg.roll = 1;
+    control_msg.pitch = velocity;
+    control_msg.yaw_rate = 1;
+    control_msg.thrust.x = thrustx;
+    control_msg.thrust.y = 0;
+    control_msg.thrust.z = 0;
 
     int count = 0;
     while (ros::ok())
@@ -81,14 +93,9 @@ int main(int argc, char **argv)
         if (tracker->current_position[2] <= 0.01 && count > 100) {
             count = 0;
             tracker->respawn();
-        }
+        }    
 
-        mav_msgs::Actuators msg;
-        float velocity = atof(argv[1]);
-
-        msg.angular_velocities = {velocity, velocity, velocity, velocity, velocity, velocity};
-
-        chatter_pub.publish(msg);
+        tracker->executeAction(control_msg);
 
         ros::spinOnce();
 
