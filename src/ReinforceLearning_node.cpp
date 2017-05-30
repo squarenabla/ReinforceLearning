0c4e2e5f64d0611b4dda4bf4652a9cffa2b639ab
Tobias Fischer <fischer_t@schlunz.mmp.intern>
2017-05-30 18:38:09 +0200

    service

--- src/ReinforceLearning_node.cpp
+++ src/ReinforceLearning_node.cpp
@@ -1,3 +1,7 @@
+#include <unistd.h>
+#include <math.h>
+
+
 #include "ros/ros.h"
 #include "mav_msgs/default_topics.h"
 #include "mav_msgs/Actuators.h"
@@ -5,7 +9,7 @@
 #include "mav_msgs/eigen_mav_msgs.h"
 #include "geometry_msgs/Pose.h"
 #include "std_srvs/Empty.h"
-
+#include "gazebo_msgs/GetModelState.h"
 
 #include <sstream>
 
@@ -14,18 +18,20 @@ class environmentTracker {
 private: ros::NodeHandle n;
     ros::Publisher firefly_motor_control_pub;
     ros::ServiceClient firefly_reset_client;
+    ros::ServiceClient get_position_client;
     ros::Subscriber firefly_position_sub;
+
 public:
-    float current_position[3];
-    float current_orientation[4];
+    double current_position[3];
+    double current_orientation[4];
 
     environmentTracker(ros::NodeHandle node) {
         n = node;
         firefly_motor_control_pub = n.advertise<mav_msgs::Actuators>("/firefly/command/motor_speed", 1000);
 
         firefly_reset_client = n.serviceClient<std_srvs::Empty>("/gazebo/reset_world");
-
-        firefly_position_sub = n.subscribe("/firefly/ground_truth/pose", 1, &environmentTracker::poseCallback, this);
+        get_position_client = n.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state");
+     //   firefly_position_sub = n.subscribe("/firefly/ground_truth/pose", 1, &environmentTracker::poseCallback, this);
 
         ros::Rate loop_rate(100);
     }
@@ -57,6 +63,33 @@ public:
                  msg->orientation.w);
     }
 
+    void getPosition() {
+        gazebo_msgs::GetModelState srv;
+        srv.request.model_name = "firefly";
+        if (get_position_client.call(srv)) {
+            ROS_INFO("Possition: %f %f %f", (float)srv.response.pose.position.x, (float)srv.response.pose.position.y, (float)srv.response.pose.position.z);
+        }
+        else {
+            ROS_ERROR("Failed to get position");
+        }
+    }
+
+    double getReward(const double targetx, const double targety, const double targetz, const int count) {
+
+
+        double difx = current_position[0] - targetx;
+        double dify = current_position[1] - targety;
+        double difz = current_position[2] - targetz;
+
+        if (current_position[2] <= 0.1 && count > 100) {
+            return 0.0;
+        }
+
+        double reward4position =  1/(difx * difx + dify * dify + difz * difz + 1);
+        double reward4orientation = 1/((current_orientation[0] * current_orientation[0] + current_orientation[1] * current_orientation[1] + current_orientation[2] * current_orientation[2])/(current_orientation[3] * current_orientation[3]) + 1);
+        return reward4position * reward4orientation;
+    }
+
 };
 
 
@@ -76,9 +109,9 @@ int main(int argc, char **argv)
 
 
     int count = 0;
-    while (ros::ok())
+    while (1)
     {
-        if (tracker->current_position[2] <= 0.01 && count > 100) {
+        if (tracker->current_position[2] <= 0.1 && count > 100) {
             count = 0;
             tracker->respawn();
         }
@@ -90,12 +123,13 @@ int main(int argc, char **argv)
 
         chatter_pub.publish(msg);
 
-        ros::spinOnce();
-
-        loop_rate.sleep();
+    //    ros::spinOnce();
+        tracker->getPosition();
+        usleep(100000);
+        //loop_rate.sleep();
         ++count;
     }
 
-
+    delete tracker;
     return 0;
 }
