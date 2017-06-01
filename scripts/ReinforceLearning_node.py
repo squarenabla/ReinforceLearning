9e65f5d3be9ac1776e33ded7e35ac8d1273dc79d
Vitaly <square.nabla@gmail.com>
2017-06-02 00:06:31 +0200

    policy optimizer graph

--- scripts/ReinforceLearning_node.py
+++ scripts/ReinforceLearning_node.py
@@ -9,6 +9,35 @@ import random
 import numpy as np
 import math
 
+
+class ComputationalGraph:
+    def __init__(self):
+        self.po_lrate = 0.02
+
+    def constructGraph(self, state_size, action_num, action_size):
+        with tf.variable_scope("policy"):
+            self.po_W1 = tf.get_variable("policy_W1", [state_size, state_size])
+            self.b_1 = tf.get_variable("policy_b1", [state_size])
+
+            self.po_W2 = tf.get_variable("policy_W2", [state_size, action_num * action_size])
+            self.b_2 = tf.get_variable("policy_b2", [action_num * action_size])
+
+            self.state = tf.placeholder(tf.float32, [None, state_size])
+            self.prev_actions = tf.placeholder(tf.float32, [None, action_num * action_size])
+            self.returnn = tf.placeholder(tf.float32, [None, 1])
+
+            self.output1 = tf.nn.sigmoid(tf.add(tf.matmul(self.state, self.po_W1), self.b_1))
+            self.output2 = tf.add(tf.matmul(self.output1, self.po_W2), self.b_2)
+
+            self.probabilities = tf.nn.softmax(tf.reshape(self.output2, [-1, action_num, action_size]))
+
+
+            self.matr_prev_actions = tf.reshape(self.prev_actions, [-1, action_num, action_size])
+            self.eligibility = tf.log(tf.reduce_sum(tf.multiply(self.matr_prev_actions, self.probabilities), -1)) * self.returnn
+            self.loss = -tf.reduce_sum(self.eligibility, -1)
+            self.po_optimizer = tf.train.AdamOptimizer(self.po_lrate).minimize(self.loss)
+
+
 class environmentState:
     def __init__(self):
         self.currentPosition = {0,0,0}
